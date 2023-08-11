17f93086643cccc85f8ebe4d2d6cf0d4881abbb4
Gobinath Rajaram <35903900+GRajaram@users.noreply.github.com>
2023-08-11 10:22:23 +0100

    Update UmailEmailServer.java

--- UmailEmailServer.java
+++ UmailEmailServer.java
@@ -1,78 +1,97 @@
-
 import java.io.*;
 import java.net.*;
 import java.util.Base64;
 
 public class UmailEmailServer {
 
-    private int port;
-    private ServerSocket serverSocket;
+    private int port;  // Server's listening port
+    private ServerSocket serverSocket;  // Socket to accept client connections
 
+    // Constructor: Initializes the server with a given port
     public UmailEmailServer(int port) {
         this.port = port;
     }
 
+    // Starts the server, listens for client connections, and handles each client in a new thread
     public void start() throws IOException {
         serverSocket = new ServerSocket(port);
         System.out.println("SMTP Server started on port " + port);
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
-            new ClientHandler(clientSocket).start();
+            new ClientHandler(clientSocket).start();  // Handle each client in a separate thread
         }
     }
 
+    // Inner class to handle client interactions
     private static class ClientHandler extends Thread {
-        private Socket socket;
-        private BufferedReader reader;
-        private PrintWriter writer;
+        private Socket socket;  // Client socket
+        private BufferedReader reader;  // For reading data from the client
+        private PrintWriter writer;  // For sending data to the client
 
+        // Constructor: Initializes the handler with the client's socket
         public ClientHandler(Socket socket) {
             this.socket = socket;
         }
 
         @Override
         public void run() {
+
             try {
+                // Setup streams for communication
                 InputStream input = socket.getInputStream();
                 OutputStream output = socket.getOutputStream();
 
                 reader = new BufferedReader(new InputStreamReader(input));
                 writer = new PrintWriter(output, true);
 
-                // Initial greeting
+                boolean isAuthenticated = false;  // Flag to check if a user is authenticated
+
+                // Initial greeting to the client
                 sendResponse("220 SimpleSMTPServer Ready");
 
                 String line;
+                // Continue reading commands from the client
                 while ((line = reader.readLine()) != null) {
                     if (line.startsWith("HELO") || line.startsWith("HELO")) {
                         sendResponse("250 Hello");
                     } else if (line.startsWith("AUTH LOGIN") || line.startsWith("AUTH")) {
-                        sendResponse("334 VXNlcm5hbWU6"); // This is "Username:" encoded in base64
+                        // Handle authentication (login)
+
+                        // Asking for username
+                        sendResponse("334 VXNlcm5hbWU6"); 
                         String encodedUsername = reader.readLine();
                         String username = new String(Base64.getDecoder().decode(encodedUsername));
 
-                        sendResponse("334 UGFzc3dvcmQ6"); // This is "Password:" encoded in base64
+                        // Asking for password
+                        sendResponse("334 UGFzc3dvcmQ6");
                         String encodedPassword = reader.readLine();
                         String password = new String(Base64.getDecoder().decode(encodedPassword));
 
+                        // Check credentials and respond accordingly
                         if (isValidCredentials(username, password)) {
                             sendResponse("235 Authentication succeeded");
+                            isAuthenticated = true;
                         } else {
                             sendResponse("535 Authentication failed");
+                            // isAuthenticated = false;
                         }
-                    } else if (line.startsWith("MAIL FROM:")) {
+                    }
+                    // Handling various SMTP commands after authentication
+                    else if (isAuthenticated && line.startsWith("MAIL FROM:")) {
                         sendResponse("250 OK");
-                    } else if (line.startsWith("RCPT TO:")) {
+                    } else if (isAuthenticated && line.startsWith("RCPT TO:")) {
                         sendResponse("250 OK");
-                    } else if (line.equals("DATA")) {
+                    } else if (isAuthenticated && line.equals("DATA")) {
                         sendResponse("354 Start mail input; end with <CRLF>.<CRLF>");
-                        readEmailData();
+                        readEmailData();  // Read email content
                         sendResponse("250 OK");
-                    } else if (line.equals("QUIT")) {
+                    } else if (isAuthenticated && line.equals("QUIT")) {
+                        // Client wants to terminate connection
                         sendResponse("221 Bye");
                         break;
                     } else {
+                        // Command not recognized
                         sendResponse("500 Unrecognized command");
                     }
                 }
@@ -80,6 +99,7 @@ public class UmailEmailServer {
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
+                // Cleanup: close the socket
                 try {
                     socket.close();
                 } catch (IOException e) {
@@ -88,32 +108,34 @@ public class UmailEmailServer {
             }
         }
 
+        // Send a response to the client
         private void sendResponse(String response) {
             writer.println(response);
         }
 
+        // Read email data from the client
         private void readEmailData() throws IOException {
             StringBuilder emailData = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
-                if (line.equals(".")) {
-                    break;
+                if (line.equals("<CRLF>.<CRLF>")) {
+                    break;  // End of email data
                 }
                 emailData.append(line).append("\n");
             }
             System.out.println("\nReceived Email:\n" + emailData);
         }
 
+        // Check if the provided credentials are valid
         private boolean isValidCredentials(String username, String password) {
-        // NOTE: This is just a hardcoded example.
-        // In a real-world application, you'd check this data against a database or other secure storage.
-        return "abc@yahoo.com".equals(username) && "12345678".equals(password);
-    }
+                CredentialVerifier verifier = new CredentialVerifier();
+                return verifier.isValidCredentials(username, password);
+        }
     }
 
-
+    // Main method to start the server
     public static void main(String[] args) {
-        UmailEmailServer server = new UmailEmailServer(25);
+        UmailEmailServer server = new UmailEmailServer(25);  // SMTP typically runs on port 25
         try {
             server.start();
         } catch (IOException e) {
