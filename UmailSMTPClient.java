c76d37dafcdb0fb3427572e31ba4b97ec0ecc94b
Gobinath Rajaram <35903900+GRajaram@users.noreply.github.com>
2023-08-11 10:24:08 +0100

    Update UmailSMTPClient.java

--- UmailSMTPClient.java
+++ UmailSMTPClient.java
@@ -3,107 +3,146 @@ import java.net.*;
 import java.util.Base64;
 import java.util.Scanner;
 
+// Define the SMTP client class
 public class UmailSMTPClient {
 
+    // Declare server details and IO stream-related instance variables
     private String smtpServer;
     private int port;
     private Socket socket;
     private BufferedReader reader;
     private PrintWriter writer;
 
+    // Constructor to initialize server address and port
     public UmailSMTPClient(String smtpServer, int port) {
         this.smtpServer = smtpServer;
         this.port = port;
     }
 
+    // Establish a connection to the SMTP server
     public void connect() throws IOException {
+        // Create a new socket for communication with the SMTP server
         socket = new Socket(smtpServer, port);
+        
+        // Initialize the reader to read data from the server
         reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
+        // Initialize the writer to send data to the server
         writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 
-        // Read the initial greeting from the SMTP server
+        // Read the server's initial greeting message
         readResponse();
     }
 
-    public void sendEmail(String username, String password, String from, String to, String subject, String body) throws IOException {
+    // Handle the process of sending an email
+    public void sendEmail(String username, String password, String from, String to, String subject, String body)
+            throws IOException {
+        // Send a HELO command to the server
         sendCommand("HELO " + smtpServer);
+        // Expect a response from the server
         readResponse();
 
-            // AUTH LOGIN command
+        try {
+            // Begin the authentication process with the server
             sendCommand("AUTH LOGIN");
-            // readResponse();
-
-            // Send username and password
+            
+            // Send the username (Base64-encoded) for authentication
             sendCommand(Base64.getEncoder().encodeToString(username.getBytes()));
-            // readResponse();
+            
+            // Send the password (Base64-encoded) for authentication
             sendCommand(Base64.getEncoder().encodeToString(password.getBytes()));
+            
+            // Expect a response after sending credentials
             readResponse();
-
-
+        } catch (Exception e) {
+            // Handle authentication errors
+            System.out.println("Invalid Username or Password, Please try again");
+            throw new IOException("SMTP server responded with an error: " + e);
+        }
+        
+        // Indicate the sender's email address
         sendCommand("MAIL FROM:<" + from + ">");
         readResponse();
 
+        // Indicate the recipient's email address
         sendCommand("RCPT TO:<" + to + ">");
         readResponse();
 
+        // Begin sending the email content
         sendCommand("DATA");
         readResponse();
 
+        // Specify email headers and the email body
         writer.println("Subject: " + subject);
         writer.println("From: " + from);
         writer.println("To: " + to);
-        writer.println();  // new line to separate headers from body
+        writer.println(); // Empty line to separate headers from body
         writer.println(body);
-        writer.println(".");
+        writer.println("<CRLF>.<CRLF>");  // End of the email data
         readResponse();
 
+        // End the SMTP session
         sendCommand("QUIT");
         readResponse();
     }
 
+    // Utility method to send commands to the SMTP server
     private void sendCommand(String command) throws IOException {
         writer.println(command);
     }
 
+    // Utility method to read and handle responses from the server
     private void readResponse() throws IOException {
         String response = reader.readLine();
         System.out.println("S: " + response);
 
-        if(response.startsWith("4") || response.startsWith("5")) {
-            
-            throw new IOException("SMTP server responded with an error: " + response);
+        // Check for error codes in the server response
+        if (response.startsWith("4") || response.startsWith("5")) {
+            if (response.startsWith("535")) {
+                System.out.println("Invalid Username or Password");
+            } else {
+                System.out.println("Error");
+            }
+            throw new IOException("Please try again");
         }
     }
 
+    // Close all established connections and IO streams
     public void close() throws IOException {
         reader.close();
         writer.close();
         socket.close();
     }
 
+    // Main method to test the SMTP client functionality
     public static void main(String[] args) {
         String smtpServer = "localhost";
         int port = 25;
 
+        // Initialize a scanner for user input
         Scanner scanner = new Scanner(System.in);
 
+        // Collect the username for SMTP authentication
         System.out.print("Enter your username: ");
         String username = scanner.nextLine();
 
+        // Collect the password for SMTP authentication
         System.out.print("Enter your password: ");
         String password = scanner.nextLine();
-
-        // System.out.print("Enter your age: ");
-        // int age = scanner.nextInt();
-
-        scanner.close();
         
-        try  {
+        scanner.close();  // Close the scanner
+
+        try {
+            // Create an instance of the SMTP client
             UmailSMTPClient smtp = new UmailSMTPClient(smtpServer, port);
-            smtp.connect();
-            smtp.sendEmail(username, password, "sender@example.com", "receiver@example.com", "Test Email", "Hello from SMTPHandler!");
+            smtp.connect();  // Connect to the server
+            
+            // Use the client to send a test email
+            smtp.sendEmail(username, password, "sender@example.com", "receiver@example.com", "Test Email",
+                    "Hello this is a test email");
         } catch (IOException e) {
-            e.printStackTrace();
+            // Handle potential IO errors
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
